b72be6d9d1b3e9b0f9d4db535d8f9e56070e3a04
facu410 <facundito410@gmail.com>
2022-06-09 19:42:34 -0300

    appv2 finished

---
+++ express-ts-api/src/app2.ts
@@ -0,0 +1,116 @@
+import express from "express";
+
+const app = express();
+
+interface BaseParams<IDType = number> {
+  id: IDType;
+}
+
+interface DogDetails {
+  name: string;
+  breed: DogBreed;
+  adopted_at: Date | null;
+  birth_date: Date | null;
+}
+
+interface APIResponse<Data> {
+  data: Data;
+  message: string;
+}
+
+interface Pagination {
+  page: number;
+  limit: number;
+  breed: DogBreed;
+}
+
+interface Empty {}
+
+type DogBreed = "labrador" | "german shepherd" | "golden retriever";
+
+type Dog = BaseParams & DogDetails;
+
+app.get<Empty, APIResponse<Dog[]>, Empty, Pagination>(
+  "/api/v1/dogs",
+  (req, res) => {
+    res.send({
+      data: [
+        {
+          name: "chiki",
+          id: 1,
+          breed: "labrador",
+          adopted_at: null,
+          birth_date: null,
+        },
+      ],
+      message: "get all dogs",
+    });
+  }
+);
+
+app.get<BaseParams, APIResponse<Dog | null>, Empty, Empty>(
+  "/api/v1/dogs/:id",
+  (req, res) => {
+    res.send({
+      data: {
+        name: "chiki",
+        id: 1,
+        breed: "labrador",
+        adopted_at: null,
+        birth_date: null,
+      },
+
+      message: "get all dogs",
+    });
+  }
+);
+
+app.post<Empty, APIResponse<Dog>, DogDetails, Empty>(
+  "/api/v1/dogs",
+  (req, res) => {
+    res.send({
+      data: {
+        name: "isi",
+        breed: "german shepherd",
+        adopted_at: null,
+        birth_date: null,
+        id: 3,
+      },
+      message: "dog added",
+    });
+    // your implementation
+  }
+);
+
+app.put<BaseParams, APIResponse<Dog>, Partial<DogDetails>, Empty>(
+  "/api/v1/dogs",
+  (req, res) => {
+    // your implementation
+    res.send({
+      data: {
+        name: "isi",
+        breed: "german shepherd",
+        adopted_at: null,
+        birth_date: null,
+        id: 3,
+      },
+      message: "dog added",
+    });
+  }
+);
+
+app.delete<BaseParams, APIResponse<Dog>, Empty, Empty>(
+  "/api/v1/dogs",
+  (req, res) => {
+    res.send({
+      data: {
+        name: "isi",
+        breed: "german shepherd",
+        adopted_at: null,
+        birth_date: null,
+        id: 3,
+      },
+      message: "dog added",
+    });
+  }
+);
